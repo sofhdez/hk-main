5784473307738aa32087eb0bd01e2c252abd8ed1
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-03-15 20:07:02 -0600

    interactivity complete

--- js/index.js
+++ js/index.js
@@ -1,53 +1,57 @@
-$(window).on('load', function () {
-
-
-    /* Splide carousels */
-    Splide.defaults = {
-        type: 'slide',
-        arrows: false,
-        autoplay: false,
-        lazyLoad: 'sequential'
-    }
-
-
-    var splideMP = new Splide('#masterplan-slider', {
-        type: 'fade',
-    });
-    splideMP.mount();
-
-
-    var splideTK = new Splide('#takealook-slider', {
-        type: 'fade'
-    });
-    splideTK.mount();
-
-    var splideHM = new Splide('#honestmaterials-slider', {
-        type: 'slide',
-        arrows: false,
-        autoplay: false,
-        interval: 8000,
-        lazyLoad: 'nearby',
-        perPage: 6,
-        perMove: 1,
-        padding: '3em',
-        gap: '1em',
-        pagination: false,
-        breakpoints: {
-            2000: {
-                destroy: true,
-            },
-            1000: {
-                perPage: 3,
-            }
-        }
-    });
-    splideHM.on('moved', function (data) {
-        $('.honestmaterials-slider-act').html(data + 1);
-    });
-    splideHM.on('mounted', function (data) {
-        $('.honestmaterials-slider-tot').html("&nbsp;|&nbsp;" + splideHM.length);
-    });
-    splideHM.mount();
-
+/* Splide carousels */
+Splide.defaults = {
+    type: 'slide',
+    arrows: false,
+    autoplay: false,
+    lazyLoad: 'sequential'
 }
-)
+
+
+var splideMP = new Splide('#masterplan-slider', {
+    type: 'fade',
+});
+splideMP.mount();
+
+/* Amenities */
+
+let isOpen = false
+let amenityCurrentlyOpen = ""
+
+$('section#amenities .mpb').on('click', function (e) {
+
+    if (isOpen && amenityCurrentlyOpen == $(this).attr('data-id')) {
+        // hide
+        isOpen = false
+        amenityCurrentlyOpen = ""
+
+        // Remove classes from the amenities section, the text and the icon
+        $('#amenities .mpb').removeClass('vis').removeClass('hid');
+        $('#amenities .mpb .text').removeClass('vis').removeClass('hid');
+        $('#amenities').removeClass();
+
+        // Add the class hid into amenities-stage and close
+        $('section#amenities .amenities-stage').addClass('hid');
+
+    } else {
+        // show
+        isOpen = true
+        amenityCurrentlyOpen = $(this).attr('data-id')
+
+        // Remove vis and hid classes from the text and the icon, in case it has
+        $('section#amenities .mpb').removeClass('vis').addClass('hid');
+        $('section#amenities .mpb .text').removeClass('vis').addClass('hid');
+
+        // Add the class vid from the li of the correspond data-id
+        $(this).removeClass('hid').addClass('vis');
+
+        // remove all classes from #amenities, in case it has
+        $('#amenities').removeClass();
+
+        // save the act num and add it to #amenities as a class
+        let idClass = $(this).attr('data-id');
+        $('#amenities').addClass('open').addClass(idClass);
+
+        // Remove hid from amenities-stage
+        $('section#amenities .amenities-stage').removeClass('hid');
+    }
+});
